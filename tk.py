afc679b240a95a32be6503c0d813e7f06301f5a2
Unidad de Sistemas OCAD <sistemas@admisionuni.edu.pe>
2023-09-29 10:32:28 -0500

    probando nueva interface 2

--- tk.py
+++ tk.py
@@ -1,4 +1,14 @@
 from tkinter import *
+
+def redimensionar():
+    print('hola')
+ 
+def crear_label():
+    v_y = v_y + 10
+    Label(frame,text='Label creada').place(x=50,y=v_y)
+ 
+
+
 root = Tk()
 root.title('Procesador de imagenes')
 root.resizable(0,0)
@@ -7,10 +17,8 @@ frame.pack()
 frame.config(width=1024,height=700)
 
 
-imagen = PhotoImage(file="assets/logo-uni.png")
-
-Label(frame,image=imagen).place(x=30,y=30)
 Label(frame,text='Redimenciona las imagnes y extrae el rostro').place(x=10,y=10)
 
+Button(frame,text='Redimensionar',command=crear_label).place(x=10,y=30)
 
 root.mainloop()
