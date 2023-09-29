fe0baff1ba7ae339e8668e851d0321d47a8cacf7
Unidad de Sistemas OCAD <sistemas@admisionuni.edu.pe>
2023-09-29 12:04:43 -0500

    Modificacion por hilos

--- init.py
+++ init.py
@@ -1,52 +1,70 @@
 import sys
+from PyQt5.QtCore import Qt, QThread, pyqtSignal
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton,QTextEdit
 
 from redimensionar import redimensiona_imagenes
+#from recortar import recorta_rostros
+
+class OpenCVThread(QThread):
+    finished = pyqtSignal()
+
+    def run(self):
+        # Tu función de OpenCV aquí
+        print('run cv2')
+        # Realiza las operaciones de OpenCV
+
+        # Emite la señal de finalización cuando termine
+        self.finished.emit()
+
+class OpenPictureThread(QThread):
+    finished = pyqtSignal(str)
+
+    def run(self):
+        # Realiza las operaciones de OpenCV
+        texto = redimensiona_imagenes()
+        # Emite la señal de finalización cuando termine
+        self.finished.emit(texto)
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Ventana Principal')
+        self.setGeometry(100, 100, 1044, 700)
+        # Crear un botón llamado "Redimensionar"
+        button_redimensionar = QPushButton('Redimensionar', self)
+        button_redimensionar.setGeometry(10, 10, 150, 30)
+
+        # Crear un botón llamado "Cortar"
+        button_cortar = QPushButton('Extrae rostro', self)
+        button_cortar.setGeometry(180, 10, 150, 30)
+
+        # Conectar el botón a la función "ejecutar_redimensionar"
+        button_redimensionar.clicked.connect(self.startPictureThread)
 
 
-def ejecutar_redimensionar():
-    text_edit.clear()
-    # Llama a la función redimensiona_imagenes desde redimensionar.py
-    mensajes = redimensiona_imagenes()
-    
-    # Agrega los mensajes al QTextEdit
-    text_edit.append("Mensajes de redimensionar_imagenes:")
-    text_edit.append(mensajes)
+        # Crea un QTextEdit para mostrar los mensajes
+        self.text_edit = QTextEdit(self)
+        self.text_edit.setGeometry(10, 50, 1024, 400)  # Ajusta la posición y el tamaño del QTextEdit
 
-def ejecutar_cortar_rostro():
-    # Llama a la función recorta_rostro desde recorte.py
-    exec(open("recorte.py").read())
-    
+    def startPictureThread(self):
+        self.thread = OpenPictureThread()
+        self.thread.finished.connect(self.onPictureFinished)
+        self.thread.start()
 
-# Crear una aplicación PyQt
-app = QApplication(sys.argv)
-
-# Crear una ventana principal
-window = QMainWindow()
-window.setWindowTitle('Ventana Principal')
-window.setGeometry(100, 100, 1044, 700)
-
-# Crear un botón llamado "Redimensionar"
-button_redimensionar = QPushButton('Redimensionar', window)
-button_redimensionar.setGeometry(10, 10, 150, 30)
-
-# Crear un botón llamado "Cortar"
-button_cortar = QPushButton('Extrae rostro', window)
-button_cortar.setGeometry(180, 10, 150, 30)
-
-# Conectar el botón a la función "ejecutar_redimensionar"
-button_redimensionar.clicked.connect(ejecutar_redimensionar)
-
-# Conectar el botón a la función "ejecutar_cortar_rostro"
-button_cortar.clicked.connect(ejecutar_cortar_rostro)
-
-# Crea un QTextEdit para mostrar los mensajes
-text_edit = QTextEdit(window)
-text_edit.setGeometry(10, 50, 1024, 400)  # Ajusta la posición y el tamaño del QTextEdit
+    def onPictureFinished(self,text):
+        # Aquí puedes realizar acciones después de que la función de OpenCV haya terminado
+        self.text_edit.append(text)
 
 
-# Mostrar la ventana principal
-window.show()
+if __name__ == '__main__':
+    # Crear una aplicación PyQt
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    # Mostrar la ventana principal
+    window.show()
 
-# Iniciar el bucle de eventos de la aplicación
-sys.exit(app.exec_())
+    # Iniciar el bucle de eventos de la aplicación
+    sys.exit(app.exec_())
--- recortar.py
+++ recortar.py
@@ -1,13 +1,11 @@
-
-import os
 import cv2
-def recorta_rostro():
+import os
+
+def recorta_rostros(args):
     # Directorio de entrada y salida
     directorio_entrada = os.getcwd()
     directorio_salida = 'recorte'
     archivo_salida = 'salida.txt'
+
     # Crea el directorio de salida si no existe
     os.makedirs(directorio_salida, exist_ok=True)
-    with open(archivo_salida, 'w') as f:
-        # Carga el clasificador de detección de rostros preentrenado
-        clasificador_rostros = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
